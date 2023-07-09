75367dfeaf29f8cab9b44c3b7ea6a35d940396b4
DhayalaCode <131718622+DhayalaCode@users.noreply.github.com>
2023-07-09 15:27:28 -0500

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -1,6 +1,7 @@
 import os
 from apikey import apikey
 import streamlit as st
+import langchain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
